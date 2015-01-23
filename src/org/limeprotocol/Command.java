9da13f4f4bdcb049c3d282fd5e8bb2a4e5e23f36
André Bires <andrebires@gmail.com>
2015-01-23 16:25:18 -0200

    Command class

---
+++ src/org/limeprotocol/Command.java
@@ -0,0 +1,11 @@
+package org.limeprotocol;
+
+import java.util.Map;
+import java.util.UUID;
+
+/**
+ * Created by and_000 on 13/01/2015.
+ */
+public class Command extends Envelope {
+
+}
--- src/org/limeprotocol/Message.java
+++ src/org/limeprotocol/Message.java
@@ -1,7 +1,10 @@
 package org.limeprotocol;
 
+import javax.activation.MimeType;
+
 /**
  * Created by and_000 on 13/01/2015.
  */
 public class Message extends Envelope {
+
 }
