d9c96ca2cb7eb65994350ebede215b6dfe8b863d
André Bires <andrebires@gmail.com>
2015-01-27 16:35:21 -0200

    Basic tcp tests

--- tcp/src/test/java/org/limeprotocol/network/tcp/TcpTransportTest.java
+++ tcp/src/test/java/org/limeprotocol/network/tcp/TcpTransportTest.java
@@ -2,14 +2,25 @@ package org.limeprotocol.network.tcp;
 
 import org.junit.Test;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import static org.junit.Assert.*;
 
 public class TcpTransportTest {
 
     @Test
-    public void test_TcpTransport(){
+    public void open_validUri_connectToRemoteNode() throws URISyntaxException, IOException {
         TcpTransport tcpTransport = new TcpTransport(null);
-        assertNotNull(tcpTransport);
+        URI serverUri = new URI("net.tcp://takenet-iris.cloudapp.net:55321");
+        tcpTransport.open(serverUri);
     }
 
+    @Test(expected = IOException.class)
+    public void open_invalidUriHostName_throwsIOException() throws URISyntaxException, IOException {
+        TcpTransport tcpTransport = new TcpTransport(null);
+        URI serverUri = new URI("net.tcp://invalidhostname.local:55321");
+        tcpTransport.open(serverUri);
+    }
 }
