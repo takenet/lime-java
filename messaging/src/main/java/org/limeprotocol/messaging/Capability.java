297e9d5d3b5d8a0b04c9d75251fe1c2f1b2ce0fd
Rafael Pacheco <ravpacheco@gmail.com>
2015-02-04 11:35:33 -0200

    Inclued Capability resource

---
+++ messaging/src/main/java/org/limeprotocol/messaging/Capability.java
@@ -0,0 +1,58 @@
+package org.limeprotocol.messaging;
+
+import org.limeprotocol.Document;
+import org.limeprotocol.MediaType;
+
+/**
+ * Represents the capabilities
+ * of the nodes of the network
+ */
+public class Capability implements Document {
+
+    public final String MIME_TYPE = "application/vnd.lime.capability+json";
+
+    public final String CONTENT_TYPES_KEY = "contentTypes";
+    public final String RESOURCE_TYPES_KEY = "resourceTypes";
+
+    public MediaType mediaType;
+
+    public Capability()
+    {
+        this.mediaType = MediaType.parse(MIME_TYPE);
+    }
+
+    /**
+     * Indicates the message content types
+     * that the session node is able to handle.
+     */
+    private MediaType[] contentTypes;
+
+    /**
+     * Indicates the command resource types
+     * that the session node is able to handle.
+     */
+    private MediaType[] resourceTypes;
+
+    public MediaType[] getContentTypes() {
+        return contentTypes;
+    }
+
+    public void setContentTypes(MediaType[] contentTypes) {
+        this.contentTypes = contentTypes;
+    }
+
+    public MediaType[] getResourceTypes() {
+        return resourceTypes;
+    }
+
+    public void setResourceTypes(MediaType[] resourceTypes) {
+        this.resourceTypes = resourceTypes;
+    }
+
+    @Override
+    public MediaType getMediaType() {
+        return this.mediaType;
+    }
+}
+
+
