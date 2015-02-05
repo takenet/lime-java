cc17431023c5b2b32787254dcca5776f37f2c4b4
Guilherme Castro <guilherme@takenet.com.br>
2015-02-05 16:48:23 -0200

    Notification serializer test

--- core/src/test/java/org/limeprotocol/testHelpers/JsonConstants.java
+++ core/src/test/java/org/limeprotocol/testHelpers/JsonConstants.java
@@ -49,7 +49,7 @@ public class JsonConstants {
         public final String RESOURCE_KEY = "resource";
         public final String METHOD_KEY = "method";
         public final String STATUS_KEY = "status";
-        public final String REASON_KEY = "reason";
+        public final String  REASON_KEY = "reason";
     }
 
     public static class Message {
@@ -58,7 +58,9 @@ public class JsonConstants {
     }
 
     public static class Notification {
-        public final String EVENT_KEY = "event";
-        public final String REASON_KEY = "reason";
+        public final static String EVENT_KEY = "event";
+        public final static String REASON_KEY = "reason";
+        public final static String CODE_FROM_REASON_KEY = REASON_KEY + "." + Reason.CODE_KEY;
+        public final static String DESCRIPTION_FROM_REASON_KEY = REASON_KEY + "." + Reason.DESCRIPTION_KEY;
     }
 }
