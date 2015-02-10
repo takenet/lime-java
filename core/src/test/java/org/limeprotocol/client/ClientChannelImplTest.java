06c1072a86fb38d51c9fb545234b2e7d8370dc98
André C. O. Minelli <andre@takenet.com.br>
2015-02-10 15:34:42 -0200

    Add tests for authenticateSession method

--- core/src/test/java/org/limeprotocol/client/ClientChannelImplTest.java
+++ core/src/test/java/org/limeprotocol/client/ClientChannelImplTest.java
@@ -2,10 +2,8 @@ package org.limeprotocol.client;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.limeprotocol.Node;
-import org.limeprotocol.Session;
-import org.limeprotocol.SessionCompression;
-import org.limeprotocol.SessionEncryption;
+import org.limeprotocol.*;
+import org.limeprotocol.security.Authentication;
 import org.limeprotocol.testHelpers.TestClientChannel;
 import org.limeprotocol.testHelpers.TestTransport;
 import org.mockito.*;
@@ -131,6 +129,109 @@ public class ClientChannelImplTest {
         fail("An UnsupportedOperationException should be threw");
     }
     //endregion negotiateSession
+
+    //region authenticateSession
+
+    @Test
+    public void authenticateSessionAsync_AuthenticatingState_CallsTransportAndReadsFromTransport() throws Exception {
+        // Arrange
+        TestClientChannel target = getTarget(SessionState.AUTHENTICATING);
+
+        Identity localIdentity = createIdentity();
+        String localInstance = createInstanceName();
+        Authentication plainAuthentication = createPlainAuthentication();
+
+        Session establishedSession = createSession(SessionState.ESTABLISHED);
+        establishedSession.setId(target.getSessionId());
+
+        transport.addNextEnvelopeToReturn(establishedSession);
+
+        ArgumentCaptor<Session> sessionArgument = ArgumentCaptor.forClass(Session.class);
+
+        // Act
+        target.authenticateSession(localIdentity, plainAuthentication, localInstance, listener);
+
+        // Assert
+        assertThat(transport.getSentEnvelopes()).hasSize(1);
+        assertThat(transport.getSentEnvelopes()[0]).isInstanceOf(Session.class);
+        Session sentSession = (Session)transport.getSentEnvelopes()[0];
+        assertThat(sentSession.getState()).isEqualTo(SessionState.AUTHENTICATING);
+        assertThat(sentSession.getFrom().toIdentity()).isEqualTo(localIdentity);
+        assertThat(sentSession.getFrom().getInstance()).isEqualTo(localInstance);
+        assertThat(sentSession.getAuthentication()).isEqualTo(plainAuthentication);
+
+        verify(listener).onReceiveSession(sessionArgument.capture());
+        Session sessionReturned = sessionArgument.getValue();
+        assertThat(sessionReturned).isNotNull().isEqualTo(establishedSession);
+    }
+
+    @Test
+    public void authenticateSessionAsync_InvalidState_ThrowsUnsupportedOperationException() throws Exception {
+        // Arrange
+        TestClientChannel target = getTarget(SessionState.ESTABLISHED);
+
+        Identity localIdentity = createIdentity();
+        String localInstance = createInstanceName();
+        Authentication plainAuthentication = createPlainAuthentication();
+
+        // Act
+        try {
+            target.authenticateSession(localIdentity, plainAuthentication, localInstance, listener);
+        } catch (UnsupportedOperationException e) {
+            // Assert
+            assertThat(transport.getSentEnvelopes()).isEmpty();
+            verify(listener, never()).onReceiveSession(any(Session.class));
+            return;
+        }
+
+        fail("An UnsupportedOperationException should be threw");
+    }
+
+    @Test
+    public void authenticateSessionAsync_NullIdentity_ThrowsIllegalArgumentException() throws Exception {
+        // Arrange
+        TestClientChannel target = getTarget(SessionState.AUTHENTICATING);
+
+        Identity localIdentity = null;
+        String localInstance = createInstanceName();
+        Authentication plainAuthentication = createPlainAuthentication();
+
+        // Act
+        try {
+            target.authenticateSession(localIdentity, plainAuthentication, localInstance, listener);
+        } catch (IllegalArgumentException e) {
+            // Assert
+            assertThat(transport.getSentEnvelopes()).isEmpty();
+            verify(listener, never()).onReceiveSession(any(Session.class));
+            return;
+        }
+
+        fail("An UnsupportedOperationException should be threw");
+    }
+
+    @Test
+    public void authenticateSessionAsync_NullAuthentication_ThrowsIllegalArgumentException() throws Exception {
+        // Arrange
+        TestClientChannel target = getTarget(SessionState.AUTHENTICATING);
+
+        Identity localIdentity = createIdentity();
+        String localInstance = createInstanceName();
+        Authentication plainAuthentication = null;
+
+        // Act
+        try {
+            target.authenticateSession(localIdentity, plainAuthentication, localInstance, listener);
+        } catch (IllegalArgumentException e) {
+            // Assert
+            assertThat(transport.getSentEnvelopes()).isEmpty();
+            verify(listener, never()).onReceiveSession(any(Session.class));
+            return;
+        }
+
+        fail("An UnsupportedOperationException should be threw");
+    }
+
+    //endregion authenticateSession
 
     private TestClientChannel getTarget() {
         return getTarget(SessionState.NEW);
