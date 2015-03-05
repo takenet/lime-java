33a678fd2db09aa16d3b26cd1f8c1c480f681dc4
André Bires <andrebires@gmail.com>
2015-03-05 14:45:22 -0300

    ReasonCodes constants are now static

--- core/src/main/java/org/limeprotocol/ReasonCodes.java
+++ core/src/main/java/org/limeprotocol/ReasonCodes.java
@@ -7,161 +7,161 @@ public class ReasonCodes {
     /**
      * General error
      */
-    public final int GENERAL_ERROR = 1;
+    public static final int GENERAL_ERROR = 1;
     /**
      * General session error
      */
-    public final int SESSION_ERROR = 11;
+    public static final int SESSION_ERROR = 11;
     /**
      * The session resource is already registered
      */
-    public final int SESSION_REGISTRATION_ERROR = 12;
+    public static final int SESSION_REGISTRATION_ERROR = 12;
     /**
      * An authentication error occurred
      */
-    public final int SESSION_AUTHENTICATION_FAILED = 13;
+    public static final int SESSION_AUTHENTICATION_FAILED = 13;
     /**
      * An error occurred while unregistering the session
      * in the server
      */
-    public final int SESSION_UNREGISTER_FAILED = 14;
+    public static final int SESSION_UNREGISTER_FAILED = 14;
     /**
      * The required action is invalid for
      * current session state
      */
-    public final int SESSION_INVALID_ACTION_FOR_STATE = 15;
+    public static final int SESSION_INVALID_ACTION_FOR_STATE = 15;
     /**
      * The session negotiation has timed out
      */
-    public final int SESSION_NEGOTIATION_TIMEOUT = 16;
+    public static final int SESSION_NEGOTIATION_TIMEOUT = 16;
     /**
      * Invalid selected negotiation options
      */
-    public final int SESSION_NEGOTIATION_INVALID_OPTIONS = 17;
+    public static final int SESSION_NEGOTIATION_INVALID_OPTIONS = 17;
     /**
      * Invalid session mode requested
      */
-    public final int SESSION_INVALID_SESSION_MODE_REQUESTED = 18;
+    public static final int SESSION_INVALID_SESSION_MODE_REQUESTED = 18;
     /**
      * General validation error
      */
-    public final int VALIDATION_ERROR = 21;
+    public static final int VALIDATION_ERROR = 21;
     /**
      * The envelope document is null
      */
-    public final int VALIDATION_EMPTY_DOCUMENT = 22;
+    public static final int VALIDATION_EMPTY_DOCUMENT = 22;
     /**
      * The envelope document MIME type is invalid
      */
-    public final int VALIDATION_INVALID_RESOURCE = 23;
+    public static final int VALIDATION_INVALID_RESOURCE = 23;
     /**
      * The request status is invalid
      */
-    public final int VALIDATION_INVALID_STATUS = 24;
+    public static final int VALIDATION_INVALID_STATUS = 24;
     /**
      * The request identity is invalid
      */
-    public final int VALIDATION_INVALID_IDENTITY = 25;
+    public static final int VALIDATION_INVALID_IDENTITY = 25;
     /**
      * The envelope originator or destination is invalid
      */
-    public final int VALIDATION_INVALID_RECIPIENTS = 26;
+    public static final int VALIDATION_INVALID_RECIPIENTS = 26;
     /**
      * General authorization error
      */
-    public final int AUTHORIZATION_ERROR = 31;
+    public static final int AUTHORIZATION_ERROR = 31;
     /**
      * The sender is not authorized to send
      * messages to the message destination
      */
-    public final int AUTHORIZATION_UNAUTHORIZED_SENDER = 32;
+    public static final int AUTHORIZATION_UNAUTHORIZED_SENDER = 32;
     /**
      * The destination doesn't have an active
      * account
      */
-    public final int AUTHORIZATION_DESTINATION_ACCOUNT_NOT_FOUND = 33;
+    public static final int AUTHORIZATION_DESTINATION_ACCOUNT_NOT_FOUND = 33;
     /**
      * General routing error
      */
-    public final int ROUTING_ERROR = 41;
+    public static final int ROUTING_ERROR = 41;
     /**
      * The message destination was not found
      */
-    public final int ROUTING_DESTINATION_NOT_FOUND = 42;
+    public static final int ROUTING_DESTINATION_NOT_FOUND = 42;
     /**
      * The message destination gateway was not found
      */
-    public final int ROUTING_GATEWAY_NOT_FOUND = 43;
+    public static final int ROUTING_GATEWAY_NOT_FOUND = 43;
     /**
      * The message destination was not found
      */
-    public final int ROUTING_ROUTE_NOT_FOUND = 44;
+    public static final int ROUTING_ROUTE_NOT_FOUND = 44;
     /**
      * General dispatching error
      */
-    public final int DISPATCH_ERROR = 51;
+    public static final int DISPATCH_ERROR = 51;
     /**
      * General command processing error
      */
-    public final int COMMAND_PROCESSING_ERROR = 61;
+    public static final int COMMAND_PROCESSING_ERROR = 61;
     /**
      * There's no command processor available
      * for process the request
      */
-    public final int COMMAND_RESOURCE_NOT_SUPPORTED = 62;
+    public static final int COMMAND_RESOURCE_NOT_SUPPORTED = 62;
     /**
      * The command method is not supported
      */
-    public final int COMMAND_METHOD_NOT_SUPPORTED = 63;
+    public static final int COMMAND_METHOD_NOT_SUPPORTED = 63;
     /**
      * The command method has an invalid argument value
      */
-    public final int COMMAND_INVALID_ARGUMENT = 64;
+    public static final int COMMAND_INVALID_ARGUMENT = 64;
     /**
      * The requested command is not valid for current
      * session mode
      */
-    public final int COMMAND_INVALID_SESSION_MODE = 65;
+    public static final int COMMAND_INVALID_SESSION_MODE = 65;
     /**
      * The command method was not allowed
      */
-    public final int COMMAND_NOT_ALLOWED = 66;
+    public static final int COMMAND_NOT_ALLOWED = 66;
     /**
      * The command resource was not found
      */
-    public final int COMMAND_RESOURCE_NOT_FOUND = 67;
+    public static final int COMMAND_RESOURCE_NOT_FOUND = 67;
     /**
      * General message processing error
      */
-    public final int MESSAGE_PROCESSING_ERROR = 61;
+    public static final int MESSAGE_PROCESSING_ERROR = 61;
     /**
      * The message content type
      * is not supported
      */
-    public final int MESSAGE_UNSUPPORTED_CONTENT_TYPE = 71;
+    public static final int MESSAGE_UNSUPPORTED_CONTENT_TYPE = 71;
     /**
      * General gateway processing error
      */
-    public final int GATEWAY_ERROR = 81;
+    public static final int GATEWAY_ERROR = 81;
     /**
      * The content type is not supported
      * by the gateway
      */
-    public final int GATEWAY_CONTENT_TYPE_NOT_SUPPORTED = 82;
+    public static final int GATEWAY_CONTENT_TYPE_NOT_SUPPORTED = 82;
     /**
      * The message destination was not found
      * on gateway
      */
-    public final int GATEWAY_DESTINATION_NOT_FOUND = 83;
+    public static final int GATEWAY_DESTINATION_NOT_FOUND = 83;
     /**
      * The functionality is not supported
      * by the gateway
      */
-    public final int GATEWAY_NOT_SUPPORTED = 84;
+    public static final int GATEWAY_NOT_SUPPORTED = 84;
     /**
      * General application processing error
      */
-    public final int APPLICATION_ERROR = 101;
+    public static final int APPLICATION_ERROR = 101;
 
 }
 
