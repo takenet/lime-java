0d8a7fe14a1277c0c2004ea26f557602b77fc1da
André Bires <andrebires@gmail.com>
2016-04-12 17:49:02 -0300

    Added DocumentContainer class. Added DocumentCollection comments.

--- core/src/main/java/org/limeprotocol/DocumentCollection.java
+++ core/src/main/java/org/limeprotocol/DocumentCollection.java
@@ -3,6 +3,9 @@ package org.limeprotocol;
 import java.util.Arrays;
 import java.util.Iterator;
 
+/**
+ * Represents a collection of documents.
+ */
 public class DocumentCollection extends DocumentBase implements Iterable {
 
     public static final String MIME_TYPE = "application/vnd.lime.collection+json";
@@ -15,26 +18,52 @@ public class DocumentCollection extends DocumentBase implements Iterable {
         super(MediaType.parse(MIME_TYPE));
     }
 
+    /**
+     * Gets or sets the total of items in the collection.
+     * The count refers to the original source collection, without any applied filter that may be applied in the items on this collection.
+     * @return
+     */
     public int getTotal() {
         return total;
     }
 
+    /**
+     * Gets or sets the total of items in the collection.
+     * The count refers to the original source collection, without any applied filter that may be applied in the items on this collection.
+     * @param total
+     */
     public void setTotal(int total) {
         this.total = total;
     }
 
+    /**
+     * Gets the media type of all items of the collection..
+     * @return
+     */
     public MediaType getItemType() {
         return itemType;
     }
 
+    /**
+     * Sets the media type of all items of the collection..
+     * @param itemType
+     */
     public void setItemType(MediaType itemType) {
         this.itemType = itemType;
     }
 
+    /**
+     * Gets the collection items.
+     * @return
+     */
     public Document[] getItems() {
         return items;
     }
 
+    /**
+     * Sets the collection items.
+     * @param items
+     */
     public void setItems(Document[] items) {
         this.items = items;
     }
---
+++ core/src/main/java/org/limeprotocol/DocumentContainer.java
@@ -0,0 +1,41 @@
+package org.limeprotocol;
+
+/**
+ * Represents a generic container for a document, providing a media type for the correct handling of its value by the nodes.
+ * This class can be used along with the DocumentCollection class to traffic different document types in a single message.
+ */
+public class DocumentContainer extends DocumentBase {
+
+    public static final String MIME_TYPE = "application/vnd.lime.container+json";
+
+    private Document value;
+
+    public DocumentContainer() {
+        super(MediaType.parse(MIME_TYPE));
+    }
+
+    /**
+     * Gets the media type of the contained document.
+     * @return
+     */
+    private MediaType getType() {
+        if (value != null) return value.getMediaType();
+        return null;
+    }
+
+    /**
+     * Gets the contained document value.
+     * @return
+     */
+    public Document getValue() {
+        return value;
+    }
+
+    /**
+     * Sets the contained document value.
+     * @param value
+     */
+    public void setValue(Document value) {
+        this.value = value;
+    }
+}
