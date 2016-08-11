29d342de43beb109de9bdc4fe99075bf42bf549b
André Bires <andrebires@gmail.com>
2016-08-11 16:14:15 -0300

    Added gender, timezone and culture account fields

--- messaging/src/main/java/org/limeprotocol/messaging/resources/Account.java
+++ messaging/src/main/java/org/limeprotocol/messaging/resources/Account.java
@@ -17,236 +17,366 @@ public class Account extends DocumentBase {
         super(MediaType.parse(MIME_TYPE));
     }
 
-    /**
-     * The user full name
-     */
     private String fullName;
-
-    /**
-     * The user address
-     */
     private String address;
-
-    /**
-     * The user city
-     */
     private String city;
-
-    /**
-     * The user e-mail address
-     */
     private String email;
-
-    /**
-     * The user phone number
-     */
     private String phoneNumber;
-
-    /**
-     * The user photo URI
-     */
     private URI photoUri;
-
-    /**
-     * The user cellphone number
-     */
     private String cellPhoneNumber;
-
-    /**
-     * Indicates that the account is
-     * temporary is valid only in
-     * the current session
-     */
     private Boolean isTemporary;
-
-    /**
-     * Base64 representation of the account password.
-     */
     private String password;
-
-    /**
-     * Base64 representation of the account password.
-     * Mandatory in case of updating account password.
-     */
     private String oldPassword;
-
-    /**
-     * Access key for updating the account without knowing the old password.
-     */
     private String accessKey;
-
-    /**
-     * Size of account inbox
-     * for storing offline messages
-     */
     private Integer inboxSize;
-
-    /**
-     * Indicates if this account
-     * allows receive messages from
-     * anonymous users
-     */
     private Boolean allowAnonymousSender;
-
-    /**
-     * Indicates if this account
-     * allows receive messages from
-     * users that are not in
-     * the account contact list
-     */
     private Boolean allowUnknownSender;
-
-    /**
-     * Indicates if the content of messages
-     * from this account should be stored in
-     * the server. Note that for offline messages,
-     * this will always happens.
-     */
     private Boolean storeMessageContent;
-
-    /**
-     * Alternative account address.
-     */
     private Identity alternativeAccount;
+    private Boolean publishToDirectory;
+    private Gender gender;
+    private Integer timezone;
+    private String culture;
 
     /**
-     * Indicates if the account info should be published to the domain directory.
+     * The user full name.
+     * @return
      */
-    private Boolean publishToDirectory;
-
-    public Boolean getAllowAnonymousSender() {
-        return allowAnonymousSender;
-    }
-
-    public void setAllowAnonymousSender(Boolean allowAnonymousSender) {
-        this.allowAnonymousSender = allowAnonymousSender;
-    }
-
-    public Boolean getAllowUnknownSender() {
-        return allowUnknownSender;
-    }
-
-    public void setAllowUnknownSender(Boolean allowUnknownSender) {
-        this.allowUnknownSender = allowUnknownSender;
-    }
-
-    public Boolean getIsTemporary() {
-        return isTemporary;
-    }
-
-    public void setIsTemporary(Boolean isTemporary) {
-        this.isTemporary = isTemporary;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getOldPassword() {
-        return oldPassword;
-    }
-
-    public void setOldPassword(String oldPassword) {
-        this.oldPassword = oldPassword;
-    }
-
-    public String getAccessKey() {
-        return accessKey;
-    }
-
-    public void setAccessKey(String accessKey) {
-        this.accessKey = accessKey;
-    }
-
-    public Boolean getStoreMessageContent() {
-        return storeMessageContent;
-    }
-
-    public void setStoreMessageContent(Boolean storeMessageContent) {
-        this.storeMessageContent = storeMessageContent;
-    }
-
-    public Integer getInboxSize() {
-        return inboxSize;
-    }
-
-    public void setInboxSize(Integer inboxSize) {
-        this.inboxSize = inboxSize;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCellPhoneNumber() {
-        return cellPhoneNumber;
-    }
-
-    public void setCellPhoneNumber(String cellPhoneNumber) {
-        this.cellPhoneNumber = cellPhoneNumber;
-    }
-
-    public URI getPhotoUri() {
-        return photoUri;
-    }
-
-    public void setPhotoUri(URI photoUri) {
-        this.photoUri = photoUri;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getFullName() {
         return fullName;
     }
 
+    /**
+     * The user full name.
+     * @param fullName
+     */
     public void setFullName(String fullName) {
         this.fullName = fullName;
     }
 
+    /**
+     * The user street address.
+     * @return
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * The user street address.
+     * @param address
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * The user city.
+     * @return
+     */
+    public String getCity() {
+        return city;
+    }
+
+    /**
+     * The user city.
+     * @param city
+     */
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    /**
+     * The user e-mail address.
+     * @return
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * The user e-mail address.
+     * @param email
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * The user phone number.
+     * @return
+     */
     public String getPhoneNumber() {
         return phoneNumber;
     }
 
+    /**
+     * The user phone number.
+     * @param phoneNumber
+     */
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
 
+    /**
+     * The user photo URI.
+     * @return
+     */
+    public URI getPhotoUri() {
+        return photoUri;
+    }
+
+    /**
+     * The user photo URI.
+     * @param photoUri
+     */
+    public void setPhotoUri(URI photoUri) {
+        this.photoUri = photoUri;
+    }
+
+    /**
+     * The user cellphone number.
+     * @return
+     */
+    public String getCellPhoneNumber() {
+        return cellPhoneNumber;
+    }
+
+    /**
+     * The user cellphone number.
+     * @param cellPhoneNumber
+     */
+    public void setCellPhoneNumber(String cellPhoneNumber) {
+        this.cellPhoneNumber = cellPhoneNumber;
+    }
+
+    /**
+     * Indicates that the account is temporary is valid only in the current session.
+     * @return
+     */
+    public Boolean getIsTemporary() {
+        return isTemporary;
+    }
+
+    /**
+     * Indicates that the account is temporary is valid only in the current session.
+     * @param temporary
+     */
+    public void setIsTemporary(Boolean temporary) {
+        isTemporary = temporary;
+    }
+
+    /**
+     * Base64 representation of the account password.
+     * @return
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * Base64 representation of the account password.
+     * @param password
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * Base64 representation of the account password.
+     * Mandatory in case of updating account password.
+     * @return
+     */
+    public String getOldPassword() {
+        return oldPassword;
+    }
+
+    /**
+     * Base64 representation of the account password.
+     * Mandatory in case of updating account password.
+     * @param oldPassword
+     */
+    public void setOldPassword(String oldPassword) {
+        this.oldPassword = oldPassword;
+    }
+
+    /**
+     * Access key for updating the account without knowing the old password.
+     * @return
+     */
+    public String getAccessKey() {
+        return accessKey;
+    }
+
+    /**
+     * Access key for updating the account without knowing the old password.
+     * @param accessKey
+     */
+    public void setAccessKey(String accessKey) {
+        this.accessKey = accessKey;
+    }
+
+    /**
+     * Size of account inbox for storing offline messages.
+     * @return
+     */
+    public Integer getInboxSize() {
+        return inboxSize;
+    }
+
+    /**
+     * Size of account inbox for storing offline messages.
+     * @param inboxSize
+     */
+    public void setInboxSize(Integer inboxSize) {
+        this.inboxSize = inboxSize;
+    }
+
+    /**
+     * Indicates if this account allows receive messages from anonymous users.
+     * @return
+     */
+    public Boolean getAllowAnonymousSender() {
+        return allowAnonymousSender;
+    }
+
+    /**
+     * Indicates if this account allows receive messages from anonymous users.
+     * @param allowAnonymousSender
+     */
+    public void setAllowAnonymousSender(Boolean allowAnonymousSender) {
+        this.allowAnonymousSender = allowAnonymousSender;
+    }
+
+    /**
+     * Indicates if this account allows receive messages from users that are not in the account contact list.
+     * @return
+     */
+    public Boolean getAllowUnknownSender() {
+        return allowUnknownSender;
+    }
+
+    /**
+     * Indicates if this account allows receive messages from users that are not in the account contact list.
+     * @param allowUnknownSender
+     */
+    public void setAllowUnknownSender(Boolean allowUnknownSender) {
+        this.allowUnknownSender = allowUnknownSender;
+    }
+
+    /**
+     * Indicates if the content of messages from this account should be stored in the server.
+     * Note that for offline messages, this will always happens.
+     * @return
+     */
+    public Boolean getStoreMessageContent() {
+        return storeMessageContent;
+    }
+
+    /**
+     * Indicates if the content of messages from this account should be stored in the server.
+     * Note that for offline messages, this will always happens.
+     * @param storeMessageContent
+     */
+    public void setStoreMessageContent(Boolean storeMessageContent) {
+        this.storeMessageContent = storeMessageContent;
+    }
+
+    /**
+     * Alternative account address.
+     * @return
+     */
     public Identity getAlternativeAccount() {
         return alternativeAccount;
     }
 
+    /**
+     * Alternative account address.
+     * @param alternativeAccount
+     */
     public void setAlternativeAccount(Identity alternativeAccount) {
         this.alternativeAccount = alternativeAccount;
     }
 
+    /**
+     * Indicates if the account info should be published to the domain directory.
+     * @return
+     */
     public Boolean getPublishToDirectory() {
         return publishToDirectory;
     }
 
+    /**
+     * Indicates if the account info should be published to the domain directory.
+     * @param publishToDirectory
+     */
     public void setPublishToDirectory(Boolean publishToDirectory) {
         this.publishToDirectory = publishToDirectory;
+    }
+
+    /**
+     * Represents the person account gender.
+     * @return
+     */
+    public Gender getGender() {
+        return gender;
+    }
+
+    /**
+     * Represents the person account gender.
+     * @param gender
+     */
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    /**
+     * Represents the account timezone relative to GMT.
+     * @return
+     */
+    public Integer getTimezone() {
+        return timezone;
+    }
+
+    /**
+     * Represents the account timezone relative to GMT.
+     * @param timezone
+     */
+    public void setTimezone(Integer timezone) {
+        this.timezone = timezone;
+    }
+
+    /**
+     * Represents the person account culture info, in the IETF language tag format.
+     * https://en.wikipedia.org/wiki/IETF_language_ta
+     * @return
+     */
+    public String getCulture() {
+        return culture;
+    }
+
+    /**
+     * Represents the person account culture info, in the IETF language tag format.
+     * https://en.wikipedia.org/wiki/IETF_language_ta
+     * @return
+     */
+    public void setCulture(String culture) {
+        this.culture = culture;
+    }
+
+
+
+    /**
+     * Represents the account person gender
+     */
+    public enum Gender {
+        /**
+         * The male gender.
+         */
+        MALE,
+        /**
+         * The female gender
+         */
+        FEMALE
     }
 }
